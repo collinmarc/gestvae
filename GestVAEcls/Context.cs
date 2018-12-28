2fd88f68466b25c08c2a8dae1c9235c94e9ef382
marc <contact@marccollin.com>
2018-12-28 13:02:43 +0100

    EF6

---
+++ GestVAEcls/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestVAEcls
+{
+    public class Context:DbContext
+    {
+        public Context() : base("name=CSGESTVAE")
+        {
+        }
+        public DbSet<Candidat> Candidats { get; set; }
+    }
+}
